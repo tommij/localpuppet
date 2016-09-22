8a922ff4e066966c9089512c044c4acedc33b7d8
Tommi Jensen <tommi.jensen@gmail.com>
2016-09-22 17:32:07 +0200

    more stuffs

--- manifests/client01.pp
+++ manifests/client01.pp
@@ -1,14 +1,4 @@
 node client01 {
-  class { 'apt':
-    update => {
-    frequency => 'daily',
-    },
-  }
 }
 node client02 {
-  class { 'apt':
-    update => {
-    frequency => 'daily',
-    },
-  }
 }
