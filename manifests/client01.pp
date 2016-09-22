af43f383d99ff8f9e8d8b9d076e63f2fcf6d4816
Tommi Jensen <tommi.jensen@gmail.com>
2016-09-22 17:23:15 +0200

    add puppetlabs-apt + daily update for client01/client02

--- manifests/client01.pp
+++ manifests/client01.pp
@@ -1,4 +1,14 @@
 node client01 {
+  class { 'apt':
+    update => {
+    frequency => 'daily',
+    },
+  }
 }
 node client02 {
+  class { 'apt':
+    update => {
+    frequency => 'daily',
+    },
+  }
 }
