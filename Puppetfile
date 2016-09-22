4766c1aca2418a9f8dda9a5cbc962a20dbbbb5f1
Tommi Jensen <tommi.jensen@gmail.com>
2016-09-22 17:21:28 +0200

    add puppetlabs-apt

--- Puppetfile
+++ Puppetfile
@@ -2,4 +2,5 @@ mod 'puppetlabs-razor'
 mod 'puppetlabs-ntp', "0.0.3"
 mod 'richardc-datacat', "0.6.1"
 mod 'puppetlabs-stdlib'
+mod 'puppetlabs-apt'
 
--- manifests/client01.pp
+++ manifests/client01.pp
@@ -1,17 +1,4 @@
 node client01 {
-  file { "/tmp/fiskening": 
-    ensure => present,
-    content => "DIIIIIMS"
-  }
-  class { '::mcollective':
-    middleware_hosts => [ 'puppet' ],
-    manage_packages => false,
-    collectives => 'rar, bar'
-  }
 }
 node client02 {
-  class { '::mcollective':
-    middleware_hosts => [ 'puppet' ],
-    manage_packages => false,
-  }
 }
