aa77ed46f8b62b5e19205af2ac496b726aadae30
Tommi Jensen <tommi.jensen@gmail.com>
2016-09-22 17:32:41 +0200

    add defaults and ntp

---
+++ manifests/defaults.pp
@@ -0,0 +1,19 @@
+class defaults () { 
+  include stages
+  class { 'apt': 
+    update => {
+    frequency => 'daily',
+    stage => first,
+    },
+  }
+  class { '::ntp' } 
+}
+
+class stages { 
+  stage { 'first': }
+  stage { 'second': }
+  stage { 'third': }
+  stage { 'last': }
+  Stage['first'] -> Stage['second'] -> Stage['third'] -> Stage['main'] -> Stage['last']
+}
+
